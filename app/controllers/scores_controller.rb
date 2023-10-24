d0bb4af264db2859a302d041b0acc3b30553c3d6
neileon81 <neileon81@gmail.com>
2023-10-24 06:47:32 -0700

    remove score editing and adding

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,5 +1,5 @@
 class ScoresController < ApplicationController
-  before_action :set_score, only: %i[ show edit update destroy ]
+  before_action :set_score, only: %i[ show destroy ]
 
   # # GET /scores or /scores.json
   # def index
